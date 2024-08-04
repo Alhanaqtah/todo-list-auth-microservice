d2d288e405a8bc8dc513c2074d7e7f4977899153
Banana Duck <a.usama@example.com>
2024-08-04 17:20:47 +0300

    auth module done

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { AuthService } from './auth.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { User } from 'src/users/user.model';
 
+@ApiTags('auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @ApiOperation({ summary: 'Регистрация пользователя' })
+    @ApiResponse({ status: 201, description: 'Пользователь успешно зарегистрирован.', type: User })
+    @Post('/signin')
+    async signin(@Body() userDto: CreateUserDto) {
+        return this.authService.createUser(userDto);
+    }
+
+    @ApiOperation({ summary: 'Вход в систему' })
+    @ApiResponse({ status: 200, description: 'Аутентификация успешна. Возвращает токен доступа.', type: 'token' })
+    @Post('/login')
+    login(@Body() userDto: CreateUserDto) {
+        return this.authService.login(userDto);
+    }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  controllers: [AuthController],
   providers: [AuthService],
-  controllers: [AuthController]
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.SECRET_KEY || 'SECRET-KEY',
+      signOptions: {expiresIn: '15d'},
+    }),
+  ],
 })
 export class AuthModule {}
